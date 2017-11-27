94eba5ebf4c033c1f8242fd4a21a9e818c2d8505
taturaja <tatu.rajaniemi@haltu.fi>
2017-11-27 14:20:21 +0200

    Define ColorSet type

--- src/colors.js
+++ src/colors.js
@@ -1,5 +1,13 @@
-export default colors = {
+export type ColorSet = {
+  max: string,
+  med: string,
+  min: string,
+}
+
+const colors: ColorSet = {
   max: '#000000',
   med: '#AAAAAA',
   min: '#FFFFFF',
 };
+
+export default colors;
