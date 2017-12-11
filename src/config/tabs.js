414744c87aebb0edb53b601852f49ff7d94d4838
taturaja <tatu.rajaniemi@haltu.fi>
2017-12-11 15:28:11 +0200

    Remove OnboardingResults tab

--- src/config/tabs.js
+++ src/config/tabs.js
@@ -1,12 +1,8 @@
 /*  @flow */
-import { OnboardingResults } from 'open-city-modules';
 import ProfileTab from 'src/components/ProfileTab';
 import { translate } from 'react-i18next';
 
 const tabs = {
-  Home: {
-    screen: OnboardingResults,
-  },
   Profile: {
     screen: translate('profileTab')(ProfileTab),
   },
