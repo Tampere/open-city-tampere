02f23887f95e99e8dd982613d2647b4795e99e98
taturaja <tatu.rajaniemi@haltu.fi>
2017-12-21 10:22:10 +0200

    Use the language from profile when profile is loaded

--- src/profile.js
+++ src/profile.js
@@ -2,6 +2,7 @@
 import { AsyncStorage } from 'react-native';
 import config from 'src/config/config.json';
 import { type Profile } from 'src/types';
+import i18n from 'src/config/translations';
 
 const key = `@${config.asyncStoragePrefix}:profile`;
 
@@ -16,7 +17,11 @@ export const saveProfile = async (profile: Profile) => {
 export const loadProfile = async () => {
   try {
     const valueJSON: string = await AsyncStorage.getItem(key);
-    return JSON.parse(valueJSON);
+    const profile = JSON.parse(valueJSON);
+    if (profile.locale) {
+      i18n.changeLanguage(profile.locale);
+    }
+    return profile;
   } catch (e) {
     console.error(`Error fetching profile from AsyncStorage: ${e.name}: ${e.message}`);
     return null;
