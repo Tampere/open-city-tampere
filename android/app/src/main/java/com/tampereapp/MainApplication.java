5318f8655e5a06a3f9c2d253c2cbb2349b9c00c9
taturaja <tatu.rajaniemi@haltu.fi>
2017-12-21 10:47:28 +0200

    Link image picker and image resizer, add android permissions.

--- android/app/src/main/java/com/tampereapp/MainApplication.java
+++ android/app/src/main/java/com/tampereapp/MainApplication.java
@@ -3,6 +3,8 @@ package com.tampereapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import fr.bamlab.rnimageresizer.ImageResizerPackage;
+import com.imagepicker.ImagePickerPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImageResizerPackage(),
+            new ImagePickerPackage(),
             new MapsPackage(),
             new VectorIconsPackage()
       );
